cd80f0380c12e8b18b2c94d45dd4c2c453b8a26d
Agung Satrio <agungsatrio541@gmail.com>
2024-08-02 00:07:42 +0700

    blade component

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,38 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman Blog</title>
-</head>
-
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <h1>Ini Halaman Blog</h1>
-
-    <section>
-        <h2>Artikel 1</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora recusandae vitae facilis dolor modi. Ab
-            quam dolor dignissimos voluptate ducimus, animi molestias assumenda repellat? Et eligendi velit recusandae
-            praesentium aperiam.</p>
-    </section>
-
-    <section>
-        <h2>Artikel 2</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora recusandae vitae facilis dolor modi. Ab
-            quam dolor dignissimos voluptate ducimus, animi molestias assumenda repellat? Et eligendi velit recusandae
-            praesentium aperiam.</p>
-    </section>
-
-    <section>
-        <h2>Artikel 3</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora recusandae vitae facilis dolor modi. Ab
-            quam dolor dignissimos voluptate ducimus, animi molestias assumenda repellat? Et eligendi velit recusandae
-            praesentium aperiam.</p>
-    </section>
-</body>
-
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Welcom to my blog</h3>
+</x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,29 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman Contact</title>
-</head>
-
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <h1>Ini Halaman Contct</h1>
-
- <ul>
-    <li>
-        <a href="https://instagram.com/agungzatrio">Instagram</a>
-    </li>
-    <li>
-        <a href="https://instagram.com/agungzatrio">Instagram</a>
-    </li>
-    <li>
-        <a href="https://instagram.com/agungzatrio">Instagram</a>
-    </li>
- </ul>
-</body>
-
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Ini Halaman Contact</h3>
+</x-layout>
