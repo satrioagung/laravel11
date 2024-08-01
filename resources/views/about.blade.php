8f160a32faf2dd6e652457eb299ba2eca62a786e
Agung Satrio <agungsatrio541@gmail.com>
2024-08-01 22:43:35 +0700

    initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman about</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <h1>Ini Halaman about</h1>
+    <h3>Owner {{ $nama }}</h3>
+</body>
+
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Contact</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <h1>Ini Halaman Contct</h1>
+
+ <ul>
+    <li>
+        <a href="https://instagram.com/agungzatrio">Instagram</a>
+    </li>
+    <li>
+        <a href="https://instagram.com/agungzatrio">Instagram</a>
+    </li>
+    <li>
+        <a href="https://instagram.com/agungzatrio">Instagram</a>
+    </li>
+ </ul>
+</body>
+
+</html>
