389c8a5d6c329a334dab47f0b517474f313e187b
Agung Satrio <agungsatrio541@gmail.com>
2024-08-02 23:47:02 +0700

    connect to db

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,44 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Agung Satrio',
-                'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor, ipsa saepe necessitatibus optio accusamus magni ad pariatur voluptas? Eius est consectetur voluptatem nam ut. Ea accusantium labore ullam ducimus. Libero!'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Agung Satrio',
-                'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor, ipsa saepe necessitatibus optio accusamus magni ad pariatur voluptas? Eius est consectetur voluptatem nam ut. Ea accusantium labore ullam ducimus. Libero!'
-            ],
-            [
-                'id' => 3,
-                'slug' => 'judul-artikel-3',
-                'title' => 'Judul Artikel 3',
-                'author' => 'Agung Satrio',
-                'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor, ipsa saepe necessitatibus optio accusamus magni ad pariatur voluptas? Eius est consectetur voluptatem nam ut. Ea accusantium labore ullam ducimus. Libero!'
-            ]
-        ];
-    }
-
-    public static function find($slug) :array
-    {
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-
-        if(!$post){
-            abort(404);
-        }
-        return $post;
-    }
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,7 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
 
-Route::get('/posts/{id}', function ($slug) {
-    $post = Post::find($slug);
-
+Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => 'Singgle post', 'post' => $post]);
 });
 
